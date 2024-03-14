505e3afb6458cb0ff2871cc14f0ffa7167cf8f5a
dsad dwsa <wertip1299@mail.ru>
2024-03-15 01:32:44 +0300

    Add workflow with PEP8 linting

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1, localhost')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1', 'localhost')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
