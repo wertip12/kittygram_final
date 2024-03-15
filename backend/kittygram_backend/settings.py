e0018304c20c21fb32e04033917c8d3fc57ee571
dsad dwsa <wertip1299@mail.ru>
2024-03-15 14:56:40 +0300

    Add workflow with PEP8 linting

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,10 @@
-# flake8: noqa
 import os
-
-from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='token')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = False
 
