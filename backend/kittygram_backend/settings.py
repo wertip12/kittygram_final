85340413cb8602a1fe9b81369d2bec52305cb7aa
dsad dwsa <wertip1299@mail.ru>
2024-03-15 19:41:45 +0300

    Add workflow with PEP8 linting

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY',default='token')
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = False
 
